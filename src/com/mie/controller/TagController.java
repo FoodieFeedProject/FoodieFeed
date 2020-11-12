ee4a8ef412d8ff7c329b822fee1010c697c0fc17
JihoonChung <cjeff000@gmail.com>
2020-11-11 22:32:36 -0500

    added visitTagPage action(?)

--- src/com/mie/controller/TagController.java
+++ src/com/mie/controller/TagController.java
@@ -3,7 +3,9 @@ package com.mie.controller;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.mie.dao.TagDao;
-import com.mie.model.Tag;
+import com.mie.dao.*;
+import com.mie.model.*;
 
 public class TagController extends HttpServlet{
 	/**
@@ -22,7 +24,8 @@ public class TagController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	
-	private static String 
+	//private static String trending = ""
+	private static String TAG_PAGE = "/tagPage.jsp";
 	
 	/*
 	private static String LIST_REVIEW_PUBLIC = "/test_ListReview.jsp";
@@ -36,15 +39,69 @@ public class TagController extends HttpServlet{
 	private static String FOODIE_FEED = "/foodieFeed.jsp";
 	*/
 	
-	private TagDao dao;
+	private TagDao tdao;
+	private ReviewDao rdao;
 
 	/**
 	 * Constructor for this class.
 	 */
 	public TagController() {
 		super();
-		dao = new TagDao();
+		tdao = new TagDao();
+		rdao = new ReviewDao();
 	}
 	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		/**
+		 * This class retrieves the appropriate 'action' found on the JSP pages:
+		 * 
+		 * - delete will direct the servlet to let the user delete a review in
+		 * the database. - create will direct the servlet to let the user add a
+		 * new review to the database. - edit will direct the servlet to let
+		 * the user edit review information in the database. - display will
+		 * direct the servlet to a review in the database. - displayFull will 
+		 * direct the servlet to the expanded review
+		 */
+		String forward = "";
+		String action = request.getParameter("action");
+		
+		if(action.equalsIgnoreCase("TrendingList")) {
+			//forward = 
+		}
+		else if(action.equalsIgnoreCase("visitTagPage")){
+			String tagname = request.getParameter("tagname");
+			List<Integer> reviewIDs = tdao.getTagReviewId(tagname);
+			
+			List<Review> tagReviews = new ArrayList<Review>();
+			
+			for(Integer i:reviewIDs) {
+				tagReviews.add(rdao.getReviewById(i));
+			}
+			forward = TAG_PAGE;
+			request.setAttribute("tagPage", tagReviews);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		tdao.getTrendingTags();
+		RequestDispatcher view = request
+				.getRequestDispatcher(LIST_tag_ADMIN);
+		request.setAttribute("tags", tdao.getAlltags());
+		view.forward(request, response);
+	}
 
 }
